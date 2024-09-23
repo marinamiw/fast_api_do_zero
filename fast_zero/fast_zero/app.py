e7a4a6ccf50eec3a09d49d0bd99a2b6e1f15ddb3
marinamiw <marinamiw2@gmail.com>
2024-09-23 10:49:00 -0300

    Melhorando cadastro (evitando repetição de engine e session)

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,44 +1,46 @@
 from http import HTTPStatus
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+
 from fastapi import FastAPI, HTTPException
 from fast_zero.models import User
 from fast_zero.schemas import UserDB, UserList, UserPublic, UserSchema, Message
 from fast_zero.settings import Settings
+from fast_zero.database import get_session
+
 app = FastAPI()
 
 database = []
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-     engine = create_engine(Settings().DATABASE_URL)#CONEXAO COM O BD
-
-     with Session(engine) as session: #iniciamos sessao
-          db_user = session.scalar(
-               select(User).where(
-                    (User.username == user.username) | (User.email == user.email)
-              )    #verifica se ja existe o username e email na base de dados
+     #iniciamos sessao
+     session = get_session()
+     db_user = session.scalar(
+          select(User).where(
+               (User.username == user.username) | (User.email == user.email)
+          )    #verifica se ja existe o username e email na base de dados
           )
-          if db_user: #caso ja exista alguem com esse username ou email
-               if db_user.username == user.username:
-                    raise HTTPException(
-                         status_code=HTTPStatus.BAD_REQUEST,
-                         detail='Username already exists',
-                    )
-               elif db_user.email == user.email:
-                    raise HTTPException(
-                         status_code=HTTPStatus.BAD_REQUEST,
-                         detail='Email already exists',
-                    )
-          #caso nao exista agora criamos o registro,passaremos tudo que nao eh init false
-          db_user = User(
-               username = user.username, 
-               email = user.email, 
-               password = user.password
+     if db_user: #caso ja exista alguem com esse username ou email
+          if db_user.username == user.username:
+               raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Username already exists',
                )
-          session.add(db_user)
-          session.commit()
-          session.refresh(db_user) #atualiza o id
+          elif db_user.email == user.email:
+               raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Email already exists',
+               )
+          #caso nao exista agora criamos o registro,passaremos tudo que nao eh init false
+     db_user = User(
+          username = user.username, 
+          email = user.email, 
+          password = user.password
+          )
+          
+     session.add(db_user)
+     session.commit()
+     session.refresh(db_user) #atualiza o id
      
      return db_user
 
